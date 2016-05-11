3db92a8f900cd76bcc011d9df217c4c0296e0d58
Jonathan Nilsfors <jonathan.nilsfors@gmail.com>
2016-05-11 18:21:20 +0200

    Server starts and returns Hello World! from root path

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 //Initial file
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var http = require('http');
+
+// configure our app to handle CORS requests
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+	next();
+});
+
+app.use(morgan('dev'));
+
+app.get('/', function(req, res){
+  console.log('Got request');
+  res.send('Hello Wordl!');
+})
+
+
+//Start the listening
+http.createServer(app).listen(8080, function(){
+  console.log('Express server listening on port 8080');
+});
