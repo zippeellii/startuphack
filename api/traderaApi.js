377cdcc47881285fc007ef88b577d6aafbeaee4a
Niklas Wärvik <niklas.warvik@gmail.com>
2016-05-14 11:00:01 +0200

    add promise to search

--- api/traderaApi.js
+++ api/traderaApi.js
@@ -1,10 +1,11 @@
 var parseString = require('xml2js').parseString;
 var fs = require('fs');
-var request = require('request')
-var config = require("../config")
+var request = require('request');
+var config = require("../config");
+var util = require('util');
 
-var traderaRequest = require('./traderaSearchRequest')
-var AdModel = require('../dbmodels/admodel')
+var traderaRequestBody = require('./traderaSearchRequest');
+var AdModel = require('../dbmodels/admodel');
 
 var post_options = {
       host: 'http://api.tradera.com',
@@ -44,27 +45,49 @@ function parseResult(response, callback) {
     });
 }
 
-function search(query, pageNumber, orderBy, callback) {
-  if(!orderBy) {
-    orderBy = "Relevance";
-  }
-  if(!pageNumber) {
-    pageNumber = 1;
-  }
+var search = function(query, searchOptions) {
 
-  var body = traderaRequest(config.traderaAppId, config.traderaAppKey, query, pageNumber, orderBy);
+  return new Promise((resolve, reject) => {
+    resolve("");
+  });
+
+  searchOptions.pageNumber = searchOptions.pageNumber || 1;
+  searchOptions.orderBy = earchOptions.orderBy || "Relevance";
+
+  var body = traderaRequestBody(config.traderaAppId, config.traderaAppKey, query, pageNumber, orderBy);
 
   traderaRequest(body, (err, result) => {
+
     if(err) {
+
       callback(err, null);
       return;
     }
 
+    //fs.writeFileSync('./data.json', util.inspect(result) , 'utf-8');
+
     var ads = result.map(item => {
+
+      var isAuction = item.ItemType[0].indexOf("Auction") > -1
+
       console.log(item);
+      var adBody = {
+        name : item.ShortDescription[0],
+        image: itemThumnailLink[0],
+        price: 77,
+        fromSite: "tradera",
+        url:"http://www.tradera.com/item/" + item.Id[0],
+        currency:"SEK",
+        isAuction: isAuction
+
+      }
+
+      var ad = new AdModel();
+
+      ad.name = item.ShortDescription[0];
     })
 
-  })
+  });
 
 }
 
@@ -73,6 +96,7 @@ function traderaRequest(body, callback) {
   options.body = body;
 
   request.post(options, (error, response, body) => {
+
     if(error || response.statusCode != 200) {
       callback(error, null);
     }
@@ -87,9 +111,13 @@ function traderaRequest(body, callback) {
   });
 }
 
-search("iphone", null, null, (err, result) => {
-  //console.log(err, result);
-  console.log(err);
+var promise = search("iphone");
+
+promise.then(res => {
+  console.log(res);
 });
 
 
+
+
+module.exports = search;
--- api/traderaSearchRequest.js
+++ api/traderaSearchRequest.js
@@ -4,7 +4,7 @@ var request = '<?xml version="1.0" encoding="utf-8"?>' +
   '<soap:Header>' + 
     '<AuthenticationHeader xmlns="http://api.tradera.com">' +
       '<AppId>[APPID]</AppId>' +
-      '<AppKey>[APPKEY]</AppKey>' +
+      '<AppKey>[APIKEY]</AppKey>' +
     '</AuthenticationHeader>' + 
     '<ConfigurationHeader xmlns="http://api.tradera.com">' +
       '<MaxResultAge>10000</MaxResultAge>' +
@@ -21,33 +21,13 @@ var request = '<?xml version="1.0" encoding="utf-8"?>' +
 '</soap:Envelope>'
 
 module.exports = function(appId, appKey, search, pageNumber, orderBy) {
+
+  console.log(appId, appKey, search, pageNumber, orderBy);
+
   return request
   .replace("[APPID]", appId)
-  .replace("[APPKEY]", appKey)
+  .replace("[APIKEY]", appKey)
   .replace("[SEARCH]", search)
   .replace("[PAGENUMBER]", pageNumber)
   .replace("[ORDERING]", orderBy);
 }
-
-
-
-example = '<?xml version="1.0" encoding="utf-8"?>' +
-'<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
-  '<soap:Header>' + 
-    '<AuthenticationHeader xmlns="http://api.tradera.com">' +
-      '<AppId>1591</AppId>' +
-      '<AppKey>44b9d001-0c93-4c9c-a6cd-0d99d3255fbe</AppKey>' +
-    '</AuthenticationHeader>' + 
-    '<ConfigurationHeader xmlns="http://api.tradera.com">' +
-      '<MaxResultAge>10000</MaxResultAge>' +
-    '</ConfigurationHeader>' +
-  '</soap:Header>' +
-  '<soap:Body>' +
-    '<Search xmlns="http://api.tradera.com">' +
-      '<query>iphone</query>' +
-      '<categoryId>0</categoryId>' +
-      '<pageNumber>1</pageNumber>' +
-      '<orderBy>Relevance</orderBy>' +
-    '</Search>' +
-  '</soap:Body>' +
-'</soap:Envelope>'
