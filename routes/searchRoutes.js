53a22b0cba25979289627b9b81bb3a6504d9e913
Jonathan Nilsfors <jonathan.nilsfors@gmail.com>
2016-05-14 08:54:16 +0200

    Route checks for minPrice and maxPrice parameters

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -6,9 +6,14 @@ module.exports = function(app, express) {
   router.route('/search')
     .get(function(req, res){
       //Must have a search query
+      //Filter variables
+      var minPrice, maxPrice;
       if(!req.params.searchQuery){
         res.status(400).send('Must specify a searchQuery');
       }
+      if(req.params.minPrice) minPrice = req.params.minPrice;
+      if(req.params.maxPrice) maxPrice = req.params.maxPrice;
+
       //Get all the data to the specified search query
       res.status(200).send('Hello Search!');
     })
