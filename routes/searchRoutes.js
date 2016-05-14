f0dc6bd6db53c05cd37b1d592eb1b33444ff061b
Jonathan Nilsfors <jonathan.nilsfors@gmail.com>
2016-05-14 08:46:55 +0200

    Add search model.

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -5,6 +5,11 @@ module.exports = function(app, express) {
 
   router.route('/search')
     .get(function(req, res){
+      //Must have a search query
+      if(!req.params.searchQuery){
+        res.status(400).send('Must specify a searchQuery');
+      }
+      //Get all the data to the specified search query
       res.status(200).send('Hello Search!');
     })
 
