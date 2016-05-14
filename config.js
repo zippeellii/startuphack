034cb1c242e522e5952da9745a7688ea51b904b5
Niklas Wärvik <niklas.warvik@gmail.com>
2016-05-14 17:52:52 +0200

    new app

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   'database': 'localhost:27018', //The database
   //'port': process.ENV.port || 8080, //Use the env port if specified
-  'traderaAppId' : 1593,
-  'traderaAppKey' : 'a5060b52-a70d-4313-b1ed-1cbd210ae271'
+  'traderaAppId' : 1594,
+  'traderaAppKey' : 'f280fc8a-346c-4bce-9a2b-bedf8b8d6a4e'
 }
